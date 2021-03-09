6b1f17041cba454d71c845ad304883cba58ead08
passy4ucj <ojinnnakapascal@gmail.com>
2021-03-09 09:52:25 +0100

    Make ID and Password not in the response

--- models/user.js
+++ models/user.js
@@ -12,6 +12,10 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
+
+    toJSON(){
+      return { ...this.get(), id: undefined, password: undefined }
+    }
   };
   User.init({
     uuid: {
